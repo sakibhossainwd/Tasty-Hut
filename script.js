5a74cfb9775c95b1b4ed20915cb01dba724f9881
sakibhossain <sakibhossainceo@gmail.com>
2023-06-19 06:34:58 -0700

    modal added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const loadMeals = (searchText) => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-    console.log(url);
+    // console.log(url);
     fetch(url)
     .then(res => res.json())
     .then(data => displayMeals(data.meals))
@@ -25,9 +25,10 @@ const displayMeals = meals => {
                               <h4 class="fw-bold">${meal.strMeal}</h4>
                                   <p class="fs-5">Category: ${meal.strCategory}</p>
                                   <p class="fs-5">Tags: ${meal.strTags}</p>
-                                  <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                  <button onclick="loadMealDetails(${meal.idMeal})" type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#mealDetails">
     View Details
   </button>
+  
                             </div>
                           </div>
                         </div>
@@ -41,31 +42,30 @@ const displayMeals = meals => {
 
 const serachMeals = () => {
     const searchText = document.getElementById('seach-Field').value;
-    console.log(searchText);
     loadMeals(searchText);
 }
 
-const loadmodaletails = async(idMeal) => {
+const loadMealDetails = idMeal => {
     const url = `
     https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}
     `
-    try{
-        const res = await fetch(url);
-        const data = await res.json();
-        didplaymodaletails(data);
-    }
-    catch(error){
-        console.log(error);
-    }
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealsDetails(data))
 }
 
-const didplaymodaletails = meal => {
-    console.log(meal)
-    document.getElementById('modaletailsLabel').innerText = meal.meals[0].strMeal;
-    const modaletails = document.getElementById('modaletailsContainer');
-    modaletails.innerHTML = `
-    <h1>Name: ${meal.idMeal}</h1>
+const displayMealsDetails = meal => {
+    const mealLabel = document.getElementById('mealDetailsLabel');
+    console.log(meal.meals[0].strMeal);
+    mealLabel.innerText = meal.meals[0].strMeal;
+    const modalDetails = document.getElementById('modal-container');
+    modalDetails.innerHTML = `
+    <h4>ID: ${meal.meals[0].idMeal}</h4> </br>
+        <h4>Category: ${meal.meals[0].strCategory}</h4> </br>
+        <img src="${meal.meals[0].strMealThumb}" class="card-img-top img-fluid">
+        <a class="btn btn-danger mt-2" href="${meal.meals[0].strYoutube}" target="_blank">Youtube: </a>
     `
 }
+
 
 loadMeals('fish')
